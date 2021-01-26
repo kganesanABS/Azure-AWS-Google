caac5d038ee6d7abd3db53a6acb5b2a7446e55e8
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-01-26 17:37:35 +0100

    Create Lambda_function2.py

---
+++ Lambda_function2.py
@@ -0,0 +1,8 @@
+import boto3
+import json
+
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('CUSTOMERS')
+    body = json.loads(event['body'])
+    table.put_item(Item = body)
+    return {"statusCode": 200, "body": "Customer Added Successfully"}
