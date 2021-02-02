a6fad8ac6e1f10a74875baa1401d04c0bca445fe
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-02-02 14:27:41 +0100

    Delete Lambda_function2.py

--- Lambda_function2.py
+++
@@ -1,7 +0,0 @@
-import boto3
-import json
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('CUSTOMERS')
-    body = json.loads(event['body'])
-    table.put_item(Item = body)
-    return {"statusCode": 200, "body": "Customer Added Successfully"}
