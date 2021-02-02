a105d0566f28888bca3916daab7cf5da575258bb
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-02-02 14:26:32 +0100

    Delete aws.py

--- handler/aws.py
+++
@@ -1,5 +0,0 @@
-import sys
-sys.path.append('Lambda_function.py)              
-import Lambda_function.py               
-def lambda_handler(event, context):
-    return response("Welcome to my python lambda function", 200)
