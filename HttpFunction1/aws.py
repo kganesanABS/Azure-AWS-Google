8f8d7b53c084e534ff91c6daa674a2d25625a57b
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-02-04 16:41:26 +0100

    Update aws.py

--- HttpFunction1/aws.py
+++ HttpFunction1/aws.py
@@ -1,3 +1,3 @@
-from AWSFunction import response            
+from httpFunction1.AWSFunction import response            
 def lambda_handler(event, context):
     return response("Welcome to my python lambda function", 200)
