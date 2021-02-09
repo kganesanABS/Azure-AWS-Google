06943590493cc297756366874172eea03fa42eab
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-02-09 17:27:58 +0100

    Update AZURE.py

--- HttpFunction1/AZURE.py
+++ HttpFunction1/AZURE.py
@@ -1,9 +1,7 @@
 import logging
-import json
+
 import azure.functions as func
 
-with open('function.json') as f:
-    data = json.load(f)
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
