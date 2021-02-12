a438ebb0e77863d839041203863f079fc05f3c6d
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-02-12 16:29:55 +0100

    Delete AWS.py

--- HttpFunction1/AWS.py
+++
@@ -1,8 +0,0 @@
-import json
-import sys
-#sys.path.append('HttpFunction1/AWSFunction')
-#from AWSFunction import response
-from HttpFunction1.AWSFunction import response
-
-def lambda_handler(event, context):
-    return response("Welcome to my python lambda function", 200)
