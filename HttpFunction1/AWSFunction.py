a8eacb48549cbf79f0650e8b9643c7cbd5e50aad
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-02-12 16:30:09 +0100

    Delete AWSFunction.py

--- HttpFunction1/AWSFunction.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def response(message, status_code):
-    return {
-        'isBase64Encoded': False,
-        'statusCode': status_code,
-        'body': json.dumps(message),
-        'headers': {'Content-Type': 'application/json'}
-    }
