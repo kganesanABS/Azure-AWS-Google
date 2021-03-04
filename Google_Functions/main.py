4404573f1867c7b2d34eb9fae8c63992c4704bd3
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-03-04 17:00:13 +0100

    Update main.py

--- Google_Functions/main.py
+++ Google_Functions/main.py
@@ -1,15 +1,14 @@
-import sys
 from flask import escape
 
 def hello_http(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Request>
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
     Returns:
         The response text, or any set of values that can be turned into a
         Response object using `make_response`
-        <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
     request_json = request.get_json(silent=True)
     request_args = request.args
