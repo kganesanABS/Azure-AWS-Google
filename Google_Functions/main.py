ccf6daaaa31e0d50eac07421701b11862bc02415
Palme240 <69111687+Palme240@users.noreply.github.com>
2021-05-21 18:25:25 +0200

    Update main.py

--- Google_Functions/main.py
+++ Google_Functions/main.py
@@ -1,4 +1,6 @@
-def hello_get(request):
+from flask import escape
+
+def hello_http(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
@@ -7,9 +9,14 @@ def hello_get(request):
         The response text, or any set of values that can be turned into a
         Response object using `make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
-    Note:
-        For more information on how Flask integrates with Cloud
-        Functions, see the `Writing HTTP functions` page.
-        <https://cloud.google.com/functions/docs/writing/http#http_frameworks>
     """
-    return 'Hello World!
+    request_json = request.get_json(silent=True)
+    request_args = request.args
+
+    if request_json and 'name' in request_json:
+        name = request_json['name']
+    elif request_args and 'name' in request_args:
+        name = request_args['name']
+    else:
+        name = 'World'
+    return 'Hello {}!'.format(escape(name))
